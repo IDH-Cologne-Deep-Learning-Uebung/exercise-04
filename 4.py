edeaed238faea5fc5b3fc1073996b7870ee7eb1b
thomas5chneider <115982322+thomas5chneider@users.noreply.github.com>
2023-11-21 19:23:44 +0100

    modified:   4.py

--- 4.py
+++ 4.py
@@ -5,7 +5,10 @@ l1 = [[int(random.normalvariate(50,20)) for x in range(random.randint(5,20))] fo
 i = 0
 for outerlist in l1:
   print(i, end=" ")
-  for element in outerlist:
-    print(element, end=" ")
+  for j in range(0,19):
+    try:
+      print(outerlist[j], end=" ")
+    except:
+      print( 0, end="  ")
   print()
   i = i + 1
